3caa3344c503ebb0e8c855a29f7dfd4fc69b7f97
barneylogo <ben.amwithyou@gmail.com>
2025-04-02 06:57:40 +0900

    Update AgentsLandListener.ts

--- back-end/src/logListeners/AgentsLandListener.ts
+++ back-end/src/logListeners/AgentsLandListener.ts
@@ -1,14 +1,1 @@
-import { Connection } from "@solana/web3.js";
-import { BN, Program } from "@coral-xyz/anchor";
-import { Pumpfun } from "../program/pumpfun";
-import {
-  CoinInfoRequest,
-  pumpProgramInterface,
-  ResultType,
-} from "../program/web3";
-import { Metaplex } from "@metaplex-foundation/js";
-// import { setCoinStatus } from "../routes/coinStatus";
 
-export class AgentsLandListener {
-  
-}
