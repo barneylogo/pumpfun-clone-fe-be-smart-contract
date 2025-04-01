7330594a3ed5d29bc9c733302ac7bc03184a38d8
barneylogo <ben.amwithyou@gmail.com>
2025-04-02 06:58:21 +0900

    update be

--- back-end/src/controller/coinController.ts
+++ back-end/src/controller/coinController.ts
@@ -1,9 +1,1 @@
-import Coin from "../models/Coin";
-import { io } from "../sockets";
 
-// @route   GET /coin/king
-// @desc    Get king of hill
-// @access  Public
-export const coinKing = async (): Promise<any> => {
-    
-}
